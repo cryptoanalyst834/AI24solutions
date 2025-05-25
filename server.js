059c149f208d0ab253353d57017000da557b2481
Svetlana <112646144+cryptoanalyst834@users.noreply.github.com>
2025-05-25 15:16:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { OpenAI } = require("openai");
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Telegraf } = require('telegraf');
